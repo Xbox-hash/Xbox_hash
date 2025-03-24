5d72dfc2bc50e08b8dd58dc5bcf1207cbb0976ad
Xbox-hash <duotech.com.py@gmail.com>
2025-03-23 21:35:55 -0300

    Primeros cambios al portfolio

---
+++ portfolio/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for portfolio project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from principal import views
+from django.contrib.auth.views import LoginView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.inicio, name="inicio"),
+    path('portfolio-details/', views.portfolio, name="portfolio"),
+    path('service-details/', views.service, name="service-details"),
+    path('starter-page/', views.starter, name="starter-page"),
+    path('login/', LoginView.as_view(template_name="login.html"), name="login"),
+    path('logout/', views.logout_view, name="logout"),
+    path('panel-administracion/', views.panel_admin, name="panel-administracion"),
+]
---
+++ principal/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
+
+def inicio(request):
+    return render(request, "index.html")
+
+def portfolio(requets):
+    return render(requets, "portfolio-details.html")
+
+def service(request):
+    return render(request, "service-details.html")
+
+def starter(request):
+    return render(request, "starter-page.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def panel_admin(request):
+    return render(request, 'panel-administracion.html')
