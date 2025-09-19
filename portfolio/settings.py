55acfd0965410c79f49908ba5054bc33e5fcf84b
Nelson Parra <duotech.com.py@gmail.com>
2025-09-18 22:39:43 -0300

    error corregido

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z8920rekk4j1@hg7hf7ufk@=^=nun*7^iv+ia)-x2=brby$py7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['xbox-hash.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
