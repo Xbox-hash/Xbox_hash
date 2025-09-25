ae74951d96e2db265378fd32868cd573478adcf8
Nelson Parra <duotech.com.py@gmail.com>
2025-09-24 22:20:20 -0300

    su

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -144,7 +144,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 # ------------------------
