0d5feee19764c2c48c7a4e65a2260c6b3ba4f1d1
Nelson Parra <duotech.com.py@gmail.com>
2025-09-24 22:24:07 -0300

    email correccion

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -139,12 +139,13 @@ LOGIN_URL = '/login/'
 # Email
 # ------------------------
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASS')
+
 
 
 # ------------------------
